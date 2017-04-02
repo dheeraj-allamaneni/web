998386edbe0792784200de9c818d75408d28ca37
Dheeraj allamaneni <a.dheeraj.95@gmail.com>
2017-04-02 14:56:51 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function box() {
+	var r = confirm("Press a button");
+if (r == true) {
+    x = "You pressed OK!";
+    location.href="appointment-done.html"
+} else {
+    x = "You pressed Cancel!";
+    
+} 
+}
